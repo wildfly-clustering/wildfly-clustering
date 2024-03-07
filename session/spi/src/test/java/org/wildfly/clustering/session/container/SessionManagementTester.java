0fa23015ad3feb2a1e0ff66a7fe38c89298f8844
Paul Ferraro <paul.ferraro@redhat.com>
2024-03-07 14:51:55 -0500

    Decrease concurrency to accommodate github actions.

--- session/spi/src/test/java/org/wildfly/clustering/session/container/SessionManagementTester.java
+++ session/spi/src/test/java/org/wildfly/clustering/session/container/SessionManagementTester.java
@@ -38,7 +38,7 @@ public class SessionManagementTester implements ClientTester, SessionManagementE
 		HEAD, GET, PUT, DELETE;
 	}
 
-	private static final int ITERATIONS = 5;
+	private static final int ITERATIONS = 3;
 	private static final int CONCURRENCY = ITERATIONS * 10;
 	private static final Duration FAILOVER_DURATION = Duration.ofSeconds(2);
 
