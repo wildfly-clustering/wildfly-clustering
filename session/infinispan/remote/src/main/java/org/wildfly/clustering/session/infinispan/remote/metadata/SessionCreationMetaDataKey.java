f4c648429b1815fcc7ae28c7be629e767789a23c
Paul Ferraro <paul.ferraro@redhat.com>
2024-03-13 13:43:19 -0400

    Replace redundant RemoteCacheKey with CacheKey.

--- session/infinispan/remote/src/main/java/org/wildfly/clustering/session/infinispan/remote/metadata/SessionCreationMetaDataKey.java
+++ session/infinispan/remote/src/main/java/org/wildfly/clustering/session/infinispan/remote/metadata/SessionCreationMetaDataKey.java
@@ -5,13 +5,13 @@
 
 package org.wildfly.clustering.session.infinispan.remote.metadata;
 
-import org.wildfly.clustering.cache.infinispan.remote.RemoteCacheKey;
+import org.wildfly.clustering.cache.infinispan.CacheKey;
 
 /**
  * Cache key for the session creation meta data entry.
  * @author Paul Ferraro
  */
-public class SessionCreationMetaDataKey extends RemoteCacheKey<String> {
+public class SessionCreationMetaDataKey extends CacheKey<String> {
 
 	public SessionCreationMetaDataKey(String id) {
 		super(id);
--- session/infinispan/remote/src/main/java/org/wildfly/clustering/session/infinispan/remote/user/UserContextKey.java
+++ session/infinispan/remote/src/main/java/org/wildfly/clustering/session/infinispan/remote/user/UserContextKey.java
@@ -5,12 +5,12 @@
 
 package org.wildfly.clustering.session.infinispan.remote.user;
 
-import org.wildfly.clustering.cache.infinispan.remote.RemoteCacheKey;
+import org.wildfly.clustering.cache.infinispan.CacheKey;
 
 /**
  * @author Paul Ferraro
  */
-public class UserContextKey extends RemoteCacheKey<String> {
+public class UserContextKey extends CacheKey<String> {
 
 	public UserContextKey(String id) {
 		super(id);
