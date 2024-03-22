97fa5b83c9d574b7433c664dd1f8e3d50eeb3c39
Paul Ferraro <paul.ferraro@redhat.com>
2024-03-22 13:48:33 -0400

    Remove unused default methods.

--- session/cache/src/main/java/org/wildfly/clustering/session/cache/user/UserSessionsFactory.java
+++ session/cache/src/main/java/org/wildfly/clustering/session/cache/user/UserSessionsFactory.java
@@ -5,8 +5,6 @@
 
 package org.wildfly.clustering.session.cache.user;
 
-import java.util.concurrent.CompletionStage;
-
 import org.wildfly.clustering.cache.CacheEntryCreator;
 import org.wildfly.clustering.cache.CacheEntryLocator;
 import org.wildfly.clustering.cache.CacheEntryRemover;
@@ -15,8 +13,4 @@ import org.wildfly.clustering.session.user.UserSessions;
 public interface UserSessionsFactory<V, D, S> extends CacheEntryCreator<String, V, Void>, CacheEntryLocator<String, V>, CacheEntryRemover<String> {
 
 	UserSessions<D, S> createUserSessions(String id, V value);
-
-	default CompletionStage<UserSessions<D, S>> createUserSessions(String id, CompletionStage<V> value) {
-		return value.thenApply(v -> this.createUserSessions(id, v));
-	}
 }
