c39ad9d73a3e379ef8c353bdc904e69a9a3544e4
Paul Ferraro <paul.ferraro@redhat.com>
2024-03-23 12:27:50 -0400

    Add utility methods to calculate the joiners/leavers of a group membership.

--- server/api/src/main/java/org/wildfly/clustering/server/GroupMembershipEvent.java
+++ server/api/src/main/java/org/wildfly/clustering/server/GroupMembershipEvent.java
@@ -5,6 +5,10 @@
 
 package org.wildfly.clustering.server;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Encapsulates a {@link GroupMembership} event.
  * @param <M> the group member type
@@ -15,4 +19,16 @@ public interface GroupMembershipEvent<M extends GroupMember> {
 	GroupMembership<M> getPreviousMembership();
 
 	GroupMembership<M> getCurrentMembership();
+
+	default Set<M> getLeavers() {
+		Set<M> members = new HashSet<>(this.getPreviousMembership().getMembers());
+		members.removeAll(this.getCurrentMembership().getMembers());
+		return Collections.unmodifiableSet(members);
+	}
+
+	default Set<M> getJoiners() {
+		Set<M> members = new HashSet<>(this.getCurrentMembership().getMembers());
+		members.removeAll(this.getPreviousMembership().getMembers());
+		return Collections.unmodifiableSet(members);
+	}
 }
