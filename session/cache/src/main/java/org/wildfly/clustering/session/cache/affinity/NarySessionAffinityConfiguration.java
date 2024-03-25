2171d18d5e92accf892fa144cd1cd66aff922b7d
Paul Ferraro <paul.ferraro@redhat.com>
2024-03-25 16:28:32 -0400

    Rename getMaxServers() -> getMaxMembers()

--- session/cache/src/main/java/org/wildfly/clustering/session/cache/affinity/NarySessionAffinityConfiguration.java
+++ session/cache/src/main/java/org/wildfly/clustering/session/cache/affinity/NarySessionAffinityConfiguration.java
@@ -6,11 +6,22 @@
 package org.wildfly.clustering.session.cache.affinity;
 
 /**
+ * Configuration of a {@link NarySessionAffinity}.
  * @author Paul Ferraro
  */
 public interface NarySessionAffinityConfiguration {
 
+	/**
+	 * The delimiter used to join members into a single value.
+	 * @return a delimiter
+	 */
 	String getDelimiter();
 
-	int getMaxServers();
+	/**
+	 * The maximum number of members to include in this affinity.
+	 * @return a maximum number of members.
+	 */
+	default int getMaxMembers() {
+		return Integer.MAX_VALUE;
+	}
 }
