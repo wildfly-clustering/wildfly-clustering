d7df96aedf24c659e96665e3c0da87a11431a558
Paul Ferraro <paul.ferraro@redhat.com>
2024-03-25 11:48:27 -0400

    Generalize singleton group membership for any group member type.

--- server/spi/src/main/java/org/wildfly/clustering/server/group/GroupMembership.java
+++ server/spi/src/main/java/org/wildfly/clustering/server/group/GroupMembership.java
@@ -5,6 +5,8 @@
 
 package org.wildfly.clustering.server.group;
 
+import java.util.List;
+
 import org.wildfly.clustering.server.GroupMember;
 
 /**
@@ -19,5 +21,38 @@ public interface GroupMembership<M extends GroupMember> extends org.wildfly.clus
 	@Override
 	default M getCoordinator() {
 		return this.getMembers().get(this.getCoordinatorIndex());
+	}
+
+	static <M extends GroupMember> GroupMembership<M> singleton(M member) {
+		List<M> members = List.of(member);
+		return new GroupMembership<>() {
+			@Override
+			public int getCoordinatorIndex() {
+				return 0;
+			}
+
+			@Override
+			public List<M> getMembers() {
+				return members;
+			}
+
+			@Override
+			public int hashCode() {
+				return this.getCoordinator().hashCode();
+			}
+
+			@Override
+			public boolean equals(Object object) {
+				if (!(object instanceof GroupMembership)) return false;
+				@SuppressWarnings("unchecked")
+				GroupMembership<M> membership = (GroupMembership<M>) object;
+				return members.equals(membership.getMembers());
+			}
+
+			@Override
+			public String toString() {
+				return members.toString();
+			}
+		};
 	}
 }
