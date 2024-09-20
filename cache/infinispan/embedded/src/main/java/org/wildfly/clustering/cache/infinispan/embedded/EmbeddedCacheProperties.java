fed450b52b81d706769d15f632ffcb1e6abdfbc1
Paul Ferraro <paul.ferraro@redhat.com>
2024-09-20 13:25:46 +0100

    Add CacheProperties.isActive().

--- cache/infinispan/embedded/src/main/java/org/wildfly/clustering/cache/infinispan/embedded/EmbeddedCacheProperties.java
+++ cache/infinispan/embedded/src/main/java/org/wildfly/clustering/cache/infinispan/embedded/EmbeddedCacheProperties.java
@@ -5,6 +5,9 @@
 
 package org.wildfly.clustering.cache.infinispan.embedded;
 
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ClusteringConfiguration;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.IsolationLevel;
 import org.infinispan.transaction.LockingMode;
@@ -16,20 +19,23 @@ import org.wildfly.clustering.cache.CacheProperties;
  */
 public class EmbeddedCacheProperties implements CacheProperties {
 
+	private final Cache<?, ?> cache;
 	private final boolean lockOnRead;
 	private final boolean lockOnWrite;
 	private final boolean marshalling;
 	private final boolean persistent;
 	private final boolean transactional;
 
-	public EmbeddedCacheProperties(Configuration config) {
-		this.transactional = config.transaction().transactionMode().isTransactional();
-		this.lockOnWrite = this.transactional && (config.transaction().lockingMode() == LockingMode.PESSIMISTIC);
-		this.lockOnRead = this.lockOnWrite && (config.locking().lockIsolationLevel() == IsolationLevel.REPEATABLE_READ);
-		boolean clustered = config.clustering().cacheMode().needsStateTransfer();
-		boolean hasStore = config.persistence().usingStores();
+	public EmbeddedCacheProperties(Cache<?, ?> cache) {
+		this.cache = cache;
+		Configuration configuration = cache.getCacheConfiguration();
+		this.transactional = configuration.transaction().transactionMode().isTransactional();
+		this.lockOnWrite = this.transactional && (configuration.transaction().lockingMode() == LockingMode.PESSIMISTIC);
+		this.lockOnRead = this.lockOnWrite && (configuration.locking().lockIsolationLevel() == IsolationLevel.REPEATABLE_READ);
+		boolean clustered = configuration.clustering().cacheMode().needsStateTransfer();
+		boolean hasStore = configuration.persistence().usingStores();
 		this.marshalling = clustered || hasStore;
-		this.persistent = clustered || (hasStore && !config.persistence().passivation()) || config.memory().isOffHeap();
+		this.persistent = clustered || (hasStore && !configuration.persistence().passivation()) || configuration.memory().isOffHeap();
 	}
 
 	@Override
@@ -55,5 +61,13 @@ public class EmbeddedCacheProperties implements CacheProperties {
 	@Override
 	public boolean isMarshalling() {
 		return this.marshalling;
+	}
+
+	@Override
+	public boolean isActive() {
+		ClusteringConfiguration clustering = this.cache.getCacheConfiguration().clustering();
+		CacheMode mode = clustering.cacheMode();
+		float capacityFactor = clustering.hash().capacityFactor();
+		return this.cache.getStatus().allowInvocations() && (!mode.isClustered() || ((capacityFactor > 0f) && (!mode.isDistributed() || (capacityFactor > Float.MIN_VALUE))));
 	}
 }
