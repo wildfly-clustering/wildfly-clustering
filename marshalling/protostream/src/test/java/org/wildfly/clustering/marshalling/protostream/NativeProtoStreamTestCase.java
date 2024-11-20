adba654c8e91c7ddaa80a1f7964bc19ae2cd9d74
Paul Ferraro <paul.ferraro@redhat.com>
2024-11-20 15:58:39 +0000

    Add test for native protostream marshalling of record.

--- marshalling/protostream/src/test/java/org/wildfly/clustering/marshalling/protostream/NativeProtoStreamTestCase.java
+++ marshalling/protostream/src/test/java/org/wildfly/clustering/marshalling/protostream/NativeProtoStreamTestCase.java
@@ -8,7 +8,7 @@ package org.wildfly.clustering.marshalling.protostream;
 import java.util.Objects;
 
 import org.infinispan.protostream.SerializationContextInitializer;
-import org.infinispan.protostream.annotations.ProtoAdapter;
+import org.infinispan.protostream.annotations.Proto;
 import org.infinispan.protostream.annotations.ProtoEnumValue;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
@@ -103,48 +103,10 @@ public class NativeProtoStreamTestCase {
 		}
 	}
 
-	static class Name {
-		private final String first;
-		private final String last;
-
-		Name(String first, String last) {
-			this.first = first;
-			this.last = last;
-		}
-
-		String getFirst() {
-			return this.first;
-		}
-
-		String getLast() {
-			return this.last;
-		}
-
-		@Override
-		public boolean equals(Object object) {
-			if (!(object instanceof Name)) return false;
-			Name name = (Name) object;
-			return Objects.equals(this.first, name.first) && Objects.equals(this.last, name.last);
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(this.last, this.first);
-		}
-
-		@Override
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			if (this.last != null) {
-				builder.append(this.last);
-				if (this.first != null) {
-					builder.append(", ").append(this.first);
-				}
-			}
-			return builder.toString();
-		}
+	@Proto
+	record Name(String first, String last) {
 	}
-
+/*
 	@ProtoAdapter(Name.class)
 	static class NameFactory {
 		@ProtoFactory
@@ -154,16 +116,16 @@ public class NativeProtoStreamTestCase {
 
 		@ProtoField(1)
 		static String getFirst(Name name) {
-			return name.getFirst();
+			return name.first();
 		}
 
 		@ProtoField(2)
 		static String getLast(Name name) {
-			return name.getLast();
+			return name.last();
 		}
 	}
-
-	@ProtoSchema(includeClasses = { Sex.class, NameFactory.class, Employee.class })
+*/
+	@ProtoSchema(includeClasses = { Sex.class, Name.class, Employee.class })
 	interface EmployeeInitializer extends SerializationContextInitializer {
 	}
 }
