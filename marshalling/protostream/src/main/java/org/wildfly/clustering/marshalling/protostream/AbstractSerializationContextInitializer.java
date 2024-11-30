f51877b4660eaca165a7efdd90d568f0a253f4cc
Paul Ferraro <paul.ferraro@redhat.com>
2024-11-30 10:35:54 +0000

    More SM fixes...

--- marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/AbstractSerializationContextInitializer.java
+++ marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/AbstractSerializationContextInitializer.java
@@ -7,6 +7,9 @@ package org.wildfly.clustering.marshalling.protostream;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 
 import org.infinispan.protostream.DescriptorParserException;
 import org.infinispan.protostream.FileDescriptorSource;
@@ -37,9 +40,7 @@ public abstract class AbstractSerializationContextInitializer implements Seriali
 	@Override
 	public void registerSchema(SerializationContext context) {
 		try {
-			context.registerProtoFiles(FileDescriptorSource.fromResources(this.loader, this.resourceName));
-		} catch (IOException e) {
-			throw new UncheckedIOException(e);
+			context.registerProtoFiles(getFileDescriptorSource(this.loader, this.resourceName));
 		} catch (DescriptorParserException e) {
 			try {
 				// If parsing failed, unregister this schema so others can register
@@ -54,5 +55,22 @@ public abstract class AbstractSerializationContextInitializer implements Seriali
 	@Override
 	public String toString() {
 		return this.resourceName;
+	}
+
+	private static FileDescriptorSource getFileDescriptorSource(ClassLoader loader, String resourceName) {
+		try {
+			return AccessController.doPrivileged(new PrivilegedExceptionAction<>() {
+				@Override
+				public FileDescriptorSource run() throws Exception {
+					return FileDescriptorSource.fromResources(loader, resourceName);
+				}
+			});
+		} catch (PrivilegedActionException e) {
+			Exception exception = e.getException();
+			if (exception instanceof IOException) {
+				throw new UncheckedIOException((IOException) exception);
+			}
+			throw new RuntimeException(exception);
+		}
 	}
 }
