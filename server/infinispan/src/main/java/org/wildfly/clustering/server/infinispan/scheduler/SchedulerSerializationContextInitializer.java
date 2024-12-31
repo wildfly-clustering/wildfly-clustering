f605a1b997498af24076c7098447c9242067cb39
Paul Ferraro <paul.ferraro@redhat.com>
2024-12-31 10:54:55 +0000

    Drop deprecated EntriesCommand

--- server/infinispan/src/main/java/org/wildfly/clustering/server/infinispan/scheduler/SchedulerSerializationContextInitializer.java
+++ server/infinispan/src/main/java/org/wildfly/clustering/server/infinispan/scheduler/SchedulerSerializationContextInitializer.java
@@ -7,7 +7,6 @@ package org.wildfly.clustering.server.infinispan.scheduler;
 
 import org.kohsuke.MetaInfServices;
 import org.wildfly.clustering.marshalling.protostream.AbstractSerializationContextInitializer;
-import org.wildfly.clustering.marshalling.protostream.ProtoStreamMarshaller;
 import org.wildfly.clustering.marshalling.protostream.Scalar;
 import org.wildfly.clustering.marshalling.protostream.SerializationContext;
 import org.wildfly.clustering.marshalling.protostream.SerializationContextInitializer;
@@ -23,6 +22,5 @@ public class SchedulerSerializationContextInitializer extends AbstractSerializat
 		context.registerMarshaller(Scalar.ANY.toMarshaller(CancelCommand.class, CancelCommand::getId, CancelCommand::new));
 		context.registerMarshaller(Scalar.ANY.toMarshaller(ContainsCommand.class, ContainsCommand::getId, ContainsCommand::new));
 		context.registerMarshaller(new ScheduleCommandMarshaller<>());
-		context.registerMarshaller(ProtoStreamMarshaller.of(new EntriesCommand<>()));
 	}
 }
