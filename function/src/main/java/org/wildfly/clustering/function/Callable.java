6d2dfdca1660699934c47367d6f09a42d4b4b2cc
Paul Ferraro <paul.ferraro@redhat.com>
2025-05-23 11:24:13 +0100

    Add Callable.of returning a fixed value.

--- function/src/main/java/org/wildfly/clustering/function/Callable.java
+++ function/src/main/java/org/wildfly/clustering/function/Callable.java
@@ -11,7 +11,12 @@ package org.wildfly.clustering.function;
  * @param <T> the result type
  */
 public interface Callable<T> extends java.util.concurrent.Callable<T> {
-
+	/**
+	 * Returns a callable whose result is mapped by the specified function.
+	 * @param <R> the mapped result type
+	 * @param mapper a mapping function
+	 * @return a callable whose result is mapped by the specified function.
+	 */
 	default <R> Callable<R> map(Function<T, R> mapper) {
 		return new Callable<>() {
 			@Override
@@ -21,6 +26,11 @@ public interface Callable<T> extends java.util.concurrent.Callable<T> {
 		};
 	}
 
+	/**
+	 * Returns a callable that runs the specified runner and returns <code>null</code>.
+	 * @param runner a runner
+	 * @return a callable that runs the specified runner and returns <code>null</code>.
+	 */
 	static Callable<Void> of(Runnable runner) {
 		return new Callable<>() {
 			@Override
@@ -31,11 +41,30 @@ public interface Callable<T> extends java.util.concurrent.Callable<T> {
 		};
 	}
 
+	/**
+	 * Returns a callable that delegates to the specified supplier.
+	 * @param supplier a supplier
+	 * @return the result of the specified supplier.
+	 */
 	static <T> Callable<T> of(Supplier<T> supplier) {
 		return new Callable<>() {
 			@Override
 			public T call() {
 				return supplier.get();
+			}
+		};
+	}
+
+	/**
+	 * Returns a callable that returns the specified value.
+	 * @param value the result value
+	 * @return a callable that returns the specified value.
+	 */
+	static <T> Callable<T> of(T value) {
+		return new Callable<>() {
+			@Override
+			public T call() {
+				return value;
 			}
 		};
 	}
