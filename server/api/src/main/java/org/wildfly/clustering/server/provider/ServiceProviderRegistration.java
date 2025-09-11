542bb354b2aa5fac4485da247c5e4b0b20177ea4
Paul Ferraro <paul.ferraro@redhat.com>
2025-09-11 10:35:22 +0100

    Extract read-only ServiceProvision interface from ServiceProviderRegistration.

--- server/api/src/main/java/org/wildfly/clustering/server/provider/ServiceProviderRegistration.java
+++ server/api/src/main/java/org/wildfly/clustering/server/provider/ServiceProviderRegistration.java
@@ -4,29 +4,15 @@
  */
 package org.wildfly.clustering.server.provider;
 
-import java.util.Set;
-
 import org.wildfly.clustering.server.GroupMember;
 import org.wildfly.clustering.server.Registration;
 
 /**
- * Registration of a provided service.
+ * Encapsulates the registration of a provisioned service.
  *
  * @author Paul Ferraro
  * @param <T> the service type
  * @param <M> the member type
  */
-public interface ServiceProviderRegistration<T, M extends GroupMember> extends Registration {
-
-	/**
-	 * The provided service.
-	 * @return a service identifier
-	 */
-	T getService();
-
-	/**
-	 * Returns the set of group members that can provide this service.
-	 * @return a set of group members
-	 */
-	Set<M> getProviders();
+public interface ServiceProviderRegistration<T, M extends GroupMember> extends ServiceProvision<T, M>, Registration {
 }
