097a066a07b484fa7fb1f7ca83bc0ce6e33e9fd0
Paul Ferraro <paul.ferraro@redhat.com>
2026-03-27 17:44:30 +0000

    Set capacity of unmarshalled map according to the number of marshalled entries.

--- marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/util/MapMarshaller.java
+++ marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/util/MapMarshaller.java
@@ -7,10 +7,12 @@ package org.wildfly.clustering.marshalling.protostream.util;
 
 import java.io.IOException;
 import java.util.AbstractMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 
 import org.infinispan.protostream.descriptors.WireType;
+import org.wildfly.clustering.function.IntFunction;
 import org.wildfly.clustering.marshalling.protostream.ProtoStreamReader;
 
 /**
@@ -22,31 +24,31 @@ import org.wildfly.clustering.marshalling.protostream.ProtoStreamReader;
  */
 public class MapMarshaller<K, V, T extends Map<K, V>> extends AbstractMapMarshaller<K, V, T> {
 
-	private final Supplier<T> factory;
+	private final IntFunction<T> factory;
 
 	/**
 	 * Creates a marshaller for a map.
 	 * @param factory a map factory
 	 */
 	@SuppressWarnings("unchecked")
-	public MapMarshaller(Supplier<T> factory) {
-		super((Class<T>) factory.get().getClass());
+	public MapMarshaller(IntFunction<T> factory) {
+		super((Class<T>) factory.apply(0).getClass());
 		this.factory = factory;
 	}
 
 	@Override
 	public T readFrom(ProtoStreamReader reader) throws IOException {
-		T map = this.factory.get();
+		List<Map.Entry<K, V>> entries = new LinkedList<>();
 		while (!reader.isAtEnd()) {
 			int tag = reader.readTag();
-			int index = WireType.getTagFieldNumber(tag);
-			switch (index) {
-				case ENTRY_INDEX -> {
-					Map.Entry<K, V> entry = reader.readObject(AbstractMap.SimpleEntry.class);
-					map.put(entry.getKey(), entry.getValue());
-				}
+			switch (WireType.getTagFieldNumber(tag)) {
+				case ENTRY_INDEX -> entries.add(reader.readObject(AbstractMap.SimpleEntry.class));
 				default -> reader.skipField(tag);
 			}
+		}
+		T map = this.factory.apply(entries.size());
+		for (Map.Entry<K, V> entry : entries) {
+			map.put(entry.getKey(), entry.getValue());
 		}
 		return map;
 	}
