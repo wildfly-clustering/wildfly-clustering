08df6488e37dda3850e3ff41ddf81f927be35643
Paul Ferraro <paul.ferraro@redhat.com>
2026-03-27 17:41:01 +0000

    Determine access order via VarHandle.

--- marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/util/LinkedHashMapMarshaller.java
+++ marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/util/LinkedHashMapMarshaller.java
@@ -6,13 +6,12 @@
 package org.wildfly.clustering.marshalling.protostream.util;
 
 import java.io.IOException;
+import java.lang.invoke.VarHandle;
 import java.util.AbstractMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
 import org.infinispan.protostream.descriptors.WireType;
 import org.wildfly.clustering.marshalling.protostream.ProtoStreamMarshaller;
@@ -30,32 +29,7 @@ public class LinkedHashMapMarshaller<K, V> extends AbstractMapMarshaller<K, V, L
 
 	private static final int ACCESS_ORDER_INDEX = ENTRY_INDEX + 1;
 
-	private static final Function<LinkedHashMap<Object, Object>, Boolean> ACCESS_ORDER = new Function<>() {
-		@Override
-		public Boolean apply(LinkedHashMap<Object, Object> map) {
-			Object insertOrder = new Object();
-			map.put(insertOrder, null);
-			try {
-				Object accessOrder = new Object();
-				map.put(accessOrder, null);
-				try {
-					// Access first inserted entry
-					// If map uses access order, this element will move to the tail of the map
-					map.get(insertOrder);
-					Iterator<Object> keys = map.keySet().iterator();
-					Object element = keys.next();
-					while ((element != insertOrder) && (element != accessOrder)) {
-						element = keys.next();
-					}
-					return element == accessOrder;
-				} finally {
-					map.remove(accessOrder);
-				}
-			} finally {
-				map.remove(insertOrder);
-			}
-		}
-	};
+	private static final VarHandle ACCESS_ORDER_HANDLE = Reflect.getVarHandle(LinkedHashMap.class, boolean.class);
 
 	@SuppressWarnings("unchecked")
 	private LinkedHashMapMarshaller() {
@@ -68,14 +42,13 @@ public class LinkedHashMapMarshaller<K, V> extends AbstractMapMarshaller<K, V, L
 		List<Map.Entry<K, V>> entries = new LinkedList<>();
 		while (!reader.isAtEnd()) {
 			int tag = reader.readTag();
-			int index = WireType.getTagFieldNumber(tag);
-			switch (index) {
+			switch (WireType.getTagFieldNumber(tag)) {
 				case ENTRY_INDEX -> entries.add(reader.readObject(AbstractMap.SimpleEntry.class));
 				case ACCESS_ORDER_INDEX -> accessOrder = reader.readBool();
 				default -> reader.skipField(tag);
 			}
 		}
-		LinkedHashMap<K, V> map = new LinkedHashMap<>(16, 0.75f, accessOrder);
+		LinkedHashMap<K, V> map = new LinkedHashMap<>(entries.size(), 0.75f, accessOrder);
 		for (Map.Entry<K, V> entry : entries) {
 			map.put(entry.getKey(), entry.getValue());
 		}
@@ -86,8 +59,7 @@ public class LinkedHashMapMarshaller<K, V> extends AbstractMapMarshaller<K, V, L
 	public void writeTo(ProtoStreamWriter writer, LinkedHashMap<K, V> map) throws IOException {
 		synchronized (map) { // Avoid ConcurrentModificationException
 			super.writeTo(writer, map);
-			@SuppressWarnings("unchecked")
-			boolean accessOrder = ACCESS_ORDER.apply((LinkedHashMap<Object, Object>) map);
+			boolean accessOrder = (Boolean) ACCESS_ORDER_HANDLE.get(map);
 			if (accessOrder) {
 				writer.writeBool(ACCESS_ORDER_INDEX, accessOrder);
 			}
