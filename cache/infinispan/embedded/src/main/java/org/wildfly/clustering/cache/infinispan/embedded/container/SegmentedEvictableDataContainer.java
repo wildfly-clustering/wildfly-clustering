bf4af2978caa31b3f7591a7bb552ecba7c431f06
Paul Ferraro <paul.ferraro@redhat.com>
2026-05-15 15:25:23 +0100

    Upgrade Infinispan to 16.1.

--- cache/infinispan/embedded/src/main/java/org/wildfly/clustering/cache/infinispan/embedded/container/SegmentedEvictableDataContainer.java
+++ cache/infinispan/embedded/src/main/java/org/wildfly/clustering/cache/infinispan/embedded/container/SegmentedEvictableDataContainer.java
@@ -95,11 +95,7 @@ public class SegmentedEvictableDataContainer<K, V> extends DefaultSegmentedDataC
 	}
 
 	@Override
-	protected void computeEntryWritten(K key, InternalCacheEntry<K, V> value) {
-		this.computeEntryWritten(this.getSegmentForKey(key), key, value);
-	}
-
-	void computeEntryWritten(int segment, K key, InternalCacheEntry<K, V> value) {
+	protected void computeEntryWritten(int segment, K key, InternalCacheEntry<K, V> value) {
 		ConcurrentMap<K, InternalCacheEntry<K, V>> map = super.getMapForSegment(segment);
 		if (map != null) {
 			map.put(key, value);
@@ -107,11 +103,7 @@ public class SegmentedEvictableDataContainer<K, V> extends DefaultSegmentedDataC
 	}
 
 	@Override
-	protected void computeEntryRemoved(K key, InternalCacheEntry<K, V> value) {
-		this.computeEntryRemoved(this.getSegmentForKey(key), key, value);
-	}
-
-	void computeEntryRemoved(int segment, K key, InternalCacheEntry<K, V> value) {
+	protected void computeEntryRemoved(int segment, K key, InternalCacheEntry<K, V> value) {
 		ConcurrentMap<K, InternalCacheEntry<K, V>> map = super.getMapForSegment(segment);
 		if (map != null) {
 			map.remove(key, value);
