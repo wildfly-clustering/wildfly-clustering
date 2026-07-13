8c0b0ce3cabaeba35541ee515d480c73420c2ce0
Paul Ferraro <paulferraro@ibm.com>
2026-07-13 16:53:27 +0100

    Refine criteria for detecting serializable lambdas.

--- marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/Privileged.java
+++ marshalling/protostream/src/main/java/org/wildfly/clustering/marshalling/protostream/Privileged.java
@@ -5,9 +5,14 @@
 
 package org.wildfly.clustering.marshalling.protostream;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.List;
+import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.function.Supplier;
 
@@ -48,5 +53,28 @@ class Privileged {
 				return ServiceLoader.load(targetClass, loader).stream().map(Supplier::get).toList();
 			}
 		});
+	}
+
+	static Optional<MethodHandle> findMethodHandle(Class<?> sourceClass, String methodName, MethodType type) {
+		if (System.getSecurityManager() == null) {
+			return findMethodHandleUnchecked(sourceClass, methodName, type);
+		}
+		return AccessController.doPrivileged(new PrivilegedAction<>() {
+			@Override
+			public Optional<MethodHandle> run() {
+				return findMethodHandleUnchecked(sourceClass, methodName, type);
+			}
+		});
+	}
+
+	private static Optional<MethodHandle> findMethodHandleUnchecked(Class<?> sourceClass, String methodName, MethodType type) {
+		try {
+			Method method = sourceClass.getDeclaredMethod(methodName, type.parameterArray());
+			return Optional.of(MethodHandles.privateLookupIn(sourceClass, MethodHandles.lookup()).unreflect(method));
+		} catch (NoSuchMethodException e) {
+			return Optional.empty();
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException(e);
+		}
 	}
 }
