c1ca6baa19ad52bc4c0658a95d10e413e063af9c
Paul Ferraro <paulferraro@ibm.com>
2026-08-12 16:43:49 +0100

    Ensure TCCL is set on write as well as read.

--- marshalling/spi/src/main/java/org/wildfly/clustering/marshalling/AbstractByteBufferMarshaller.java
+++ marshalling/spi/src/main/java/org/wildfly/clustering/marshalling/AbstractByteBufferMarshaller.java
@@ -5,6 +5,11 @@
 
 package org.wildfly.clustering.marshalling;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.OptionalInt;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 import org.wildfly.clustering.context.Context;
@@ -22,7 +27,37 @@ public abstract class AbstractByteBufferMarshaller implements ByteBufferMarshall
 	}
 
 	@Override
-	public Supplier<Context> getContextProvider() {
-		return this.contextProvider;
+	public Object read(ByteBuffer buffer) throws IOException {
+		try (Context context = this.contextProvider.get()) {
+			return ByteBufferMarshaller.super.read(buffer);
+		}
 	}
-}
+
+	@Override
+	public ByteBuffer write(Object object) throws IOException {
+		try (Context context = this.contextProvider.get()) {
+			return ByteBufferMarshaller.super.write(object);
+		}
+	}
+
+	@Override
+	public OptionalInt size(Object object) {
+		AtomicInteger size = new AtomicInteger();
+		OutputStream output = new OutputStream() {
+			@Override
+			public void write(int b) {
+				size.setPlain(size.getPlain() + 1);
+			}
+
+			@Override
+			public void write(byte[] bytes, int offset, int length) {
+				size.setPlain(size.getPlain() + length);
+			}
+		};
+		try (Context context = this.contextProvider.get()) {
+			this.writeTo(output, object);
+			return OptionalInt.of(size.getPlain());
+		} catch (Throwable e) {
+			return OptionalInt.empty();
+		}
+	}}
