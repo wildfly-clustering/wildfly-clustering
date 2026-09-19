48f1bfc554368d8c058440f74defefeccd44b7bc
Paul Ferraro <paulferraro@ibm.com>
2026-09-19 09:43:48 +0100

    Optimise marshalling of put-if-absent and compute-if-absent functions.

--- cache/spi/src/main/java/org/wildfly/clustering/cache/function/FunctionSerializationContextInitializer.java
+++ cache/spi/src/main/java/org/wildfly/clustering/cache/function/FunctionSerializationContextInitializer.java
@@ -5,6 +5,8 @@
 
 package org.wildfly.clustering.cache.function;
 
+import java.util.function.Function;
+
 import org.kohsuke.MetaInfServices;
 import org.wildfly.clustering.marshalling.protostream.AbstractSerializationContextInitializer;
 import org.wildfly.clustering.marshalling.protostream.Scalar;
@@ -25,11 +27,14 @@ public class FunctionSerializationContextInitializer extends AbstractSerializati
 		// Do nothing
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void registerMarshallers(SerializationContext context) {
 		context.registerMarshaller(MapComputeFunctionMarshaller.INSTANCE);
 		context.registerMarshaller(new CollectionFunctionMarshaller<>(SetAddFunction.class, SetAddFunction::new));
 		context.registerMarshaller(new CollectionFunctionMarshaller<>(SetRemoveFunction.class, SetRemoveFunction::new));
 		context.registerMarshaller(Scalar.ANY.toMarshaller(RemappingFunction.class, RemappingFunction::getOperand, RemappingFunction::new));
+		context.registerMarshaller(Scalar.ANY.cast((Class<Function<? super Object, ? extends Object>>) (Class<?>) Function.class).toMarshaller(ComputeIfAbsentFunction.class, ComputeIfAbsentFunction::getDefaultValueFactory, ComputeIfAbsentFunction::new));
+		context.registerMarshaller(Scalar.ANY.toMarshaller(PutIfAbsentFunction.class, PutIfAbsentFunction::getDefaultValue, PutIfAbsentFunction::new));
 	}
 }
